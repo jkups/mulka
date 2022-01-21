ee5d6b9dc3eca73cbcdbd8ba3e8498e8f689da73
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-01-22 10:36:37 +1100

    Add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: [],
+  content: [
+    "./app/components/**/*.rb",
+    "./app/**/*.html.erb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.ts",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        104: "24rem",
+        108: "26rem",
+      },
+    },
   },
   plugins: [],
 };

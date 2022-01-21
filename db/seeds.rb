2e11bc0891bdd9a0c8e3a0121d1f94e6b4e9af84
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-01-22 10:38:13 +1100

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,62 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+PROPERTY_TYPE = ["Apartment", "Terrace", "House"].freeze
+PROPERTY_IMAGE = [
+  "na8vluep3ak42mhnbnj1",
+  "qgaacrsaccdesfbhix5i",
+  "gkjnuyskk7dctedu8odl",
+  "vyi5xafgrugdtuduswio"
+]
+
+user = User.create!(
+  email: "one@example.com",
+  password: "password",
+  password_confirmation: "password"
+)
+p "user was successfully created"
+
+3.times do |idx|
+  property = Property.create!(
+    pid: "PID" + (rand * 1000).round.to_s,
+    status: 1,
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    listing_price: Faker::Number.number(digits: 6),
+    total_units: 1000,
+    available_units: 1000,
+    minimum_units: 1,
+    description: Faker::Restaurant.description,
+    image: PROPERTY_IMAGE[idx]
+  )
+
+  PropertyFeature.create!(
+    property: property,
+    bed_count: (1..4).to_a.sample,
+    bath_count: (1..3).to_a.sample,
+    carpark: (1..2).to_a.sample,
+    plot_size: 3500,
+    prop_type: PROPERTY_TYPE.sample
+  )
+
+  2.times do
+    unit_price = property.listing_price / property.total_units
+    units_to_acquire = Faker::Number.within(range: 1..10)
+    value = unit_price * units_to_acquire
+
+    Transaction.create!(
+      units: units_to_acquire,
+      value: value,
+      fee: value * 0.1,
+      trxn_number: Faker::Number.number(digits: 6),
+      property: property,
+      user: user
+    )
+  end
+end
+
+p "properties and trasacntions were successfully created"
+
+Account.create!(
+  name: "Diamond",
+  number: Faker::Number.number(digits: 8),
+  user: user
+)
+p "account was sucessfully created"

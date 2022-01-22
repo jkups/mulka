eac130804433da10c852969b77903aafc0b366c2
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-01-22 13:59:07 +1100

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  # Scan for N+1 queries in development
+  if Rails.env.development?
+    before_action do
+      Prosopite.scan
+    end
+
+    after_action do
+      Prosopite.finish
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    portfolios_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end

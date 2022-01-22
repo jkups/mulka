f97689c002e332c6c4aadf6bcf261703a5bd495a
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-01-22 14:33:28 +1100

    Update dotenv intializer

--- config/initializers/dotenv.rb
+++
@@ -1,7 +0,0 @@
-# # Raise if variables not set
-Dotenv.require_keys(
-  "CLOUDINARY_NAME", "CLOUDINARY_API_KEY", "CLOUDINARY_API_SECRET"
-)
-
-# # parse a list of env files for programmatic inspection without modifying the ENV
-Dotenv.parse(".env.local")

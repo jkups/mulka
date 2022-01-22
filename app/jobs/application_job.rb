31fee54d6556a6da013da06456b2396e7e579f4f
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-01-22 13:59:39 +1100

    Update application job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,7 +1,12 @@
 class ApplicationJob < ActiveJob::Base
-  # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
+  retry_on StandardError,
+    wait: :exponentially_longer,
+    attempts: 3
 
-  # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
+  around_perform do |job, block|
+    block.call
+  rescue => e # rescues StandardError and subclasses
+    puts e
+    raise
+  end
 end

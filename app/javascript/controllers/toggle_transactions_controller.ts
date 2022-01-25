877c83c0d0dba42c84f1321a95d53ded0b8a23f5
John Kupoluyi <john.kupoluyi@weployapp.com>
2022-01-25 18:13:08 +1100

    Update transaction toggle controller

--- app/javascript/controllers/toggle_transactions_controller.ts
+++ app/javascript/controllers/toggle_transactions_controller.ts
@@ -1,5 +1,18 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  toggle() {}
+  connect(): void {
+    console.log("am here");
+  }
+  toggle(event) {
+    event.preventDefault();
+
+    const element = event.currentTarget as HTMLElement;
+    const elements = document.querySelectorAll(
+      `[data-id="${element.dataset.id}"`
+    );
+    const targetContainer = elements[elements.length - 1];
+    targetContainer.classList.toggle("hidden");
+    element.classList.toggle("rotate-180");
+  }
 }
